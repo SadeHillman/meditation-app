a16365ee243bfc8232ada170e406bf1724388295
SadeHillman <sadehillman@hotmail.com>
2023-06-04 01:03:01 +1200

    declaring variables

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = () => {
+  const song = document.querySelector('.song')
+  const play = document.querySelector('.play')
+  const outline = document.querySelector('.moving-outline circle')
+  const video = document.querySelector('.vid container video')
+
+  //Sounds
+  const sounds = document.querySelectorAll('.sound-picker button')
+  //Time Display
+  const timeDisplay = document.querySelectorAll('.time-display')
+  // Get the Length of the Outline
+  const outlineLength = outline.getTotalLength()
+}
+app()
